aaededf78d7a27c98ba9af791e351060e086dcc9
jpark1607 <jpark1607@gmail.com>
2020-01-27 16:05:39 +0900

    picnic completed

--- picnic.cc
+++ picnic.cc
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <list>
+#include <iterator>
 using namespace std;
 
-bool findFriend(list<pair<int, int>>::iterator iter);
+void findFriend(list<pair<int, int> >::iterator iter, list<pair<int, int> >::iterator end_iter);
 
+int C = 0, c = 0; // # of testcase, max 50
 int N = 0, n = 0; // # of students, max 10
 int M = 0, m = 0; // # of friend pairs, max n * (n - 1) / 2
 int f1 = 0, f2 = 0; // # for friend pair idx
 bool checked[10] = {false, };
-  
+int cnt = 0;
+
 int main(void) {
-  int C = 0, c = 0; // # of testcase, max 50
-  int cnt = 0;
-  /* Get # of testcases */
   cin >> C;
 
   for (c = 0; c < C; c++) {
-    list<pair<int, int>> Pair;
-    /* Get # of students and # of friend pairs */
+    list<pair<int, int> > Pair;
     cin >> N >> M;
 
     cnt = 0;
@@ -29,34 +28,42 @@ int main(void) {
       Pair.push_back(make_pair(f1, f2));
     }
 
-    auto begin_iter = Pair.begin();
-    for (m = 0; m < M; m++) {
-      if (findFriend(begin_iter) == true)
-        cnt++;
-    }
+    auto iter = Pair.begin();
+    auto end_iter = Pair.end();
+
+    findFriend(iter, end_iter);
     
-    
+    cout << cnt << endl;
   }
 
   return 0;
 }
 
 
-bool findFriend(list<pair<int, int>>::iterator iter) {
+void findFriend(list<pair<int, int> >::iterator iter, list<pair<int, int> >::iterator end_iter) {
   bool found = true;
+  
   for (n = 0; n < N; n++) {
     if (checked[n] == false)
       found = false;
   }
-  if (found == true)
-    return true;
+  if (found == true) {
+    cnt++;
+    return;
+  }
 
-  /* go find */
-
-  /* check result */
-  if (findFriend(iter) == true)
-    return true;
-  else
-    return false;
+  for (; iter != end_iter; ++iter) {
+    int first = (*iter).first;
+    int second = (*iter).second;
+    
+    if (checked[first] == false && checked[second] == false) {
+      checked[first] = true;
+      checked[second] = true;
+      findFriend(iter, end_iter);
+      checked[first] = false;
+      checked[second] = false;
+    }
+  }
   
+  return;
 }
