7a9c832bb4c80167f016469b61da4afb062fc3e6
jpark1607 <jpark1607@gmail.com>
2020-10-01 14:31:18 +0900

    fanmeeting, not started yet

---
+++ fanmeeting.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* TODO */
+int main(void) {
+	int C = 0, c = 0;
+
+	cin >> C;
+
+	for (c = 0; c < C; c++) {
+
+
+	  cout << 1 << endl;
+	}
+	return 0;
+}
